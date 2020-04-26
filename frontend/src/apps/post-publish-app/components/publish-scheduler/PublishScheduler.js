ccffc1a45373cc3211f2f2fc5f10c8875c4f4d2d
vanquyettran <vanquyettran@hotmail.com>
2020-04-26 16:45:42 +0700

    PostPublishApp/PublishScheduler: add RadioGroupInput and show 2 options publish now and scheduling

--- frontend/src/apps/post-publish-app/components/publish-scheduler/PublishScheduler.js
+++ frontend/src/apps/post-publish-app/components/publish-scheduler/PublishScheduler.js
@@ -1,14 +1,27 @@
 import './PublishScheduler.less';
 import React from 'react';
+import {translate} from '../../../../i18n';
+import RadioGroupInput from '../../../../components/radio-group-input/RadioGroupInput';
 import DateInput from '../../../../components/date-input/DateInput';
 import TimeInput from '../../../../components/time-input/TimeInput';
 
 export default class PublishScheduler extends React.Component {
-    render() {
-        return <div className="publish-time-scheduler">
+
+    getDateTimeInput = () => {
+        return <div className="date-time">
             <TimeInput/>
-            <br/>
             <DateInput/>
+        </div>;
+    };
+
+    render() {
+        return <div className="publish-scheduler">
+            <RadioGroupInput
+                options={[
+                    {value: false, label: translate('Publish now')},
+                    {value: true, label: this.getDateTimeInput()},
+                ]}
+            />
         </div>;
     }
 }
